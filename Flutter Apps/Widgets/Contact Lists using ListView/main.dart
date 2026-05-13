8a7e1bf4ddde4b984a64c744d38d1c9f38ce438f
Ahsan-Ali <149705869+Ahsanali18@users.noreply.github.com>
2026-05-13 23:22:42 +0500

    Add files via upload

---
+++ Flutter Apps/Widgets/Contact Lists using ListView/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final List<Map<String, String>> contacts = [
+    {
+      "name": "Ahsan Ali",
+      "phone": "03XX-XXXXXXXX",
+      "image": "https://robohash.org/1.png"
+    },
+    {
+      "name": "Jibran Shaikh",
+      "phone": "0311-9876543",
+      "image": "https://robohash.org/2.png"
+    },
+    {
+      "name": "Dhani Bux",
+      "phone": "0333-5678901",
+      "image": "https://robohash.org/3.png"
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("Contacts List")),
+        body: ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            var contact = contacts[index];
+            return Card(
+              margin: EdgeInsets.all(10),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(contact["image"]!),
+                ),
+                title: Text(contact["name"]!),
+                subtitle: Text(contact["phone"]!),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
